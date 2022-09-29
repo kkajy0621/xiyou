181c4f3b31dcbc9119109caef4c965a2aff61099
kkajy0621 <811051558@qq.com>
2022-09-29 23:53:06 +0800

    关闭指引皇宫交任务传送

--- fqxy/template/xy668.php
+++ fqxy/template/xy668.php
@@ -10,6 +10,7 @@ $cdid[]=$cmid;
 $clj[]=20;
 $npc[]=187;
 echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>传送</font></a>"."<br>";
+/*
 echo "<font color=black>2.皇宫交任务</font>";
 //cmd及超链接值
 $cmid=$cmid+1;
@@ -17,7 +18,8 @@ $cdid[]=$cmid;
 $clj[]=20;
 $npc[]=188;
 echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>传送</font></a>"."<br>";
-echo "<font color=black>3.每日交任务</font>";
+ */
+echo "<font color=black>2.每日交任务</font>";
 //cmd及超链接值
 $cmid=$cmid+1;
 $cdid[]=$cmid;
@@ -26,7 +28,7 @@ $npc[]=189;
 echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>传送</font></a>"."<br>";
 echo "<font color=red>【我要西游和法宝声望】</font>"."<br>";
 
-echo "<font color=black>4.恶魔广场</font>";
+echo "<font color=black>3.恶魔广场</font>";
 //cmd及超链接值
 $cmid=$cmid+1;
 $cdid[]=$cmid;
@@ -35,14 +37,14 @@ $npc[]=190;
 echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>传送</font></a>"."<br>";
 
 echo "<font color=red>【我要财宝】</font>"."<br>";
-echo "<font color=black>5.许愿池</font>";
+echo "<font color=black>4.许愿池</font>";
 //cmd及超链接值
 $cmid=$cmid+1;
 $cdid[]=$cmid;
 $clj[]=20;
 $npc[]=191;
 echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>传送</font></a>"."<br>";
-echo "<font color=black>6.金银山</font>";
+echo "<font color=black>5.金银山</font>";
 //cmd及超链接值
 $cmid=$cmid+1;
 $cdid[]=$cmid;
@@ -50,7 +52,7 @@ $clj[]=20;
 $npc[]=192;
 echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>传送</font></a>"."<br>";
 
-echo "<font color=black>7.伏羲阵</font>";
+echo "<font color=black>6.伏羲阵</font>";
 //cmd及超链接值
 $cmid=$cmid+1;
 $cdid[]=$cmid;
@@ -59,7 +61,7 @@ $npc[]=192;
 echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>传送</font></a>"."<br>";
 
 echo "<font color=red>【宝石顶级宝石】</font>"."<br>";
-echo "<font color=black>8.镶嵌宝石升级，宠物星符练级</font>";
+echo "<font color=black>7.镶嵌宝石升级，宠物星符练级</font>";
 //cmd及超链接值
 $cmid=$cmid+1;
 $cdid[]=$cmid;
@@ -67,7 +69,7 @@ $clj[]=20;
 $npc[]=193;
 echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>传送</font></a>"."<br>";
 echo "<font color=red>【我要金豆无数】</font>"."<br>";
-echo "<font color=black>9.天蓬元帅</font>";
+echo "<font color=black>8.天蓬元帅</font>";
 //cmd及超链接值
 $cmid=$cmid+1;
 $cdid[]=$cmid;
@@ -76,7 +78,7 @@ $npc[]=193;
 echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>传送</font></a>"."<br>";
 
 echo "<font color=red>【我要参与节日活动】</font>"."<br>";
-echo "<font color=black>10.玄武大街</font>";
+echo "<font color=black>9.玄武大街</font>";
 //cmd及超链接值
 $cmid=$cmid+1;
 $cdid[]=$cmid;
@@ -85,7 +87,7 @@ $npc[]=194;
 echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>传送</font></a>"."<br>";
 
 echo "<font color=red>【我要打遍天下】</font>"."<br>";
-echo "<font color=black>11.擂台</font>";
+echo "<font color=black>10.擂台</font>";
 //cmd及超链接值
 $cmid=$cmid+1;
 $cdid[]=$cmid;
@@ -93,7 +95,7 @@ $clj[]=402;
 $npc[]=0;
 echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>传送</font></a>"."<br>";
 echo "<font color=red>【我要各种钱锭】</font>"."<br>";
-echo "<font color=black>12.钱老板</font>";
+echo "<font color=black>11.钱老板</font>";
 //cmd及超链接值
 $cmid=$cmid+1;
 $cdid[]=$cmid;
@@ -102,7 +104,7 @@ $npc[]=195;
 echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>传送</font></a>"."<br>";
 
 echo "<font color=red>【我要争夺花魁】</font>"."<br>";
-echo "<font color=black>13.百花仙子（15:00开启）</font>";
+echo "<font color=black>12.百花仙子（15:00开启）</font>";
 //cmd及超链接值
 $cmid=$cmid+1;
 $cdid[]=$cmid;
@@ -111,7 +113,7 @@ $npc[]=195;
 echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>传送</font></a>"."<br>";
 
 echo "<font color=red>【我要西游声望】</font>"."<br>";
-echo "<font color=black>15.青龙大街</font>";
+echo "<font color=black>13.青龙大街</font>";
 //cmd及超链接值
 $cmid=$cmid+1;
 $cdid[]=$cmid;
@@ -120,7 +122,7 @@ $npc[]=196;
 echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>传送</font></a>"."<br>";
 
 echo "<font color=red>【我要娱乐】</font>"."<br>";
-echo "<font color=black>16.猜拳、泡泡、红包挖宝、财神、幸运大富翁</font>";
+echo "<font color=black>14.猜拳、泡泡、红包挖宝、财神、幸运大富翁</font>";
 //cmd及超链接值
 $cmid=$cmid+1;
 $cdid[]=$cmid;
@@ -128,7 +130,7 @@ $clj[]=403;
 $npc[]=0;
 echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>传送</font></a>"."<br>";
 echo "<font color=red>【我要结婚】</font>"."<br>";
-echo "<font color=black>17.月宫</font>";
+echo "<font color=black>15.月宫</font>";
 //cmd及超链接值
 $cmid=$cmid+1;
 $cdid[]=$cmid;
@@ -136,18 +138,18 @@ $clj[]=20;
 $npc[]=197;
 echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>传送</font></a>"."<br>";
 echo "<font color=red>【我要打世界boss】</font>"."<br>";
-echo "<font color=black>19.【腾云】查看</font>"."<br>";
+echo "<font color=black>16.【腾云】查看</font>"."<br>";
 
 echo "<font color=red>【我要挑战】</font>"."<br>";
-echo "<font color=black>19.【火热玩法查看】查看</font>"."<br>";
+echo "<font color=black>17.【火热玩法查看】查看</font>"."<br>";
 
 echo "<font color=red>【我要免费午餐】</font>"."<br>";
-echo "<font color=black>20.每日12点-13点（天宫秘宝）21:30点-22点（长安秘宝）</font>"."<br>";
+echo "<font color=black>18.每日12点-13点（天宫秘宝）21:30点-22点（长安秘宝）</font>"."<br>";
 
 echo "<font color=red>【我要打造装备】</font>"."<br>";
-echo "<font color=black>21.长安十字-张果老-对应门派区域传送点打造第一件装备</font>"."<br>";
-echo "<font color=black>22.所有装备穿在身上后直接升级升星</font>"."<br>";
-echo "<font color=black>23.法宝坐骑披风天宫往右走</font>";
+echo "<font color=black>19.长安十字-张果老-对应门派区域传送点打造第一件装备</font>"."<br>";
+echo "<font color=black>20.所有装备穿在身上后直接升级升星</font>"."<br>";
+echo "<font color=black>21.法宝坐骑披风天宫往右走</font>";
 //cmd及超链接值
 $cmid=$cmid+1;
 $cdid[]=$cmid;
@@ -169,8 +171,3 @@ $cdid[]=$cmid;
 $clj[]=2;
 $npc[]=0;
 echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>返回游戏</font></a>"."<br>";
-
-
-
-
-?>
