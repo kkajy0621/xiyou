7d58d737333ad70394727cabf1a3ee9fafcde425
kkajy0621 <811051558@qq.com>
2022-09-29 21:04:23 +0800

    修复GM后台部分功能

--- fqxy/admin/gm63.php
+++ fqxy/admin/gm63.php
@@ -12,16 +12,16 @@ $m=0;
 
  while(!!$row=mysql_fetch_array($result)){
 $q2="all_user";
-$sql1=mysql_query("select wjid from $q2 where wjid=$row['wjid']",$conn);
+$sql1=mysql_query("select wjid from $q2 where wjid=$row[wjid]",$conn);
 $info1=@mysql_fetch_array($sql1);
-$fh=$info1['wjid'];
+$fh=$info1[wjid];
 if($fh >=1){
-echo "<font color=blue>【违规封号】玩家【:".$row['username']."（". $row['wjid']."）】====充值总额：【".$row['vipjy']."】</font></br>";
+echo "<font color=blue>【违规封号】玩家【:".$row[username]."（". $row[wjid]."）】====充值总额：【".$row[vipjy]."】</font></br>";		
 } else{	
-if($row['vipjy'] >=1){
-echo "<font color=red>【YES】玩家【:".$row['username']."（". $row['wjid']."）】====充值总额：【".$row['vipjy']."】</font></br>";
+if($row[vipjy] >=1){	
+echo "<font color=red>【YES】玩家【:".$row[username]."（". $row[wjid]."）】====充值总额：【".$row[vipjy]."】</font></br>";	
 } else{
-echo "<font color=balck>【NO】玩家【:".$row['username']."（". $row['wjid']."）】====充值总额：【".$row['vipjy']."】</font></br>";
+echo "<font color=balck>【NO】玩家【:".$row[username]."（". $row[wjid]."）】====充值总额：【".$row[vipjy]."】</font></br>";			
 } 	
 
 } 
@@ -30,7 +30,7 @@ echo "<font color=balck>【NO】玩家【:".$row['username']."（". $row['wjid']
 }
 
 
-echo "<a href='gm.php?wjid=$wjiddd&&pass=$password&&gid=3'><font color=blue>【返回上级】</font></a>"."<br>";
+echo "<a href='gm.php?gid=3'><font color=blue>【返回上级】</font></a>"."<br>";
 
 
 
