56e95e1a994b73476ac31ce4492bc28ffbf454ba
kkajy0621 <811051558@qq.com>
2022-09-30 00:42:45 +0800

    更新装备攻略

--- fqxy/template/xy406.php
+++ fqxy/template/xy406.php
@@ -1,23 +1,118 @@
 <?php
 
-
-
-
 echo "<font color=red>幻想西游攻略-装备篇</font>"."<br>";
-echo "<font color=black>幻想西游共有3个层次的装备：1.新手套装；2.师门套装；3.幻想套装</font>"."<br>";
+echo "<font color=black>幻想西游人物共有3个层次的装备：1.新手套装；2.师门套装；3.幻想套装</font>"."<br>";
 echo "<font color=black>1.新手套装：新手套装即〖玄冰套装〗，1级可用，不能升级。</font>"."<br>";
 echo "<font color=black>2.师门套装：各师门打造，60级可用，可以升级，十字张果老处传送至相应的门派。</font>"."<br>";
-echo "<font color=black>（1）将军府：【白虎套装】在将军府白发老人（装备打造）处打造。</font>"."<br>";
-echo "<font color=black>（2）龙宫：【龙鳞套装】在龙宫龟八斗（装备打造）处打造。</font>"."<br>";
-echo "<font color=black>（3）月宫：【碧海套装】在月宫月宫管家麻姑（装备打造）处打造。</font>"."<br>";
-echo "<font color=black>（5）普陀山：【伏虎套装】在普陀山惠岸行者（装备打造）处打造。</font>"."<br>";
-echo "<font color=black>3.幻想套装：【幻想套装】由充值活动获得（详见游戏首页充值奖励），各师门通用，50级可穿戴，可以升级。</font>"."<br>";
-echo "<font color=black>4.幻想婚饰：十字张果老处传送至月宫再往左边走一步。</font>"."<br>";
+echo "<font color=black>3.幻想套装：【幻想套装】商城购买获得，各师门通用，50级可穿戴，可以升级。</font>"."<br>";
+echo "<font color=black>4.婚饰装备：十字张果老处传送至月宫再往左边走一步。</font>"."<br>";
 echo "<font color=black>5.法宝装备：十字张果老处传送至天宫再往右边走一步。</font>"."<br>";
-
-
+echo "<font color=black>6.宠物装备：十字张果老处传送至天宫-宠行天下（宠物装备商）处购买。</font>"."<br>";
+echo "<font color=black>7.人物时装：十字往上-龟仙人（时装兑换）处兑换获得。</font>"."<br>";
+echo "<font color=black>（1）将军府：【白虎套装】在将军府-白发老人（装备打造）处打造。</font>"."<br>";
+echo "<font color=black>（2）龙宫：【龙鳞套装】在龙宫-龟八斗（装备打造）处打造。</font>"."<br>";
+echo "<font color=black>（3）月宫：【碧海套装】在月宫-月宫管家麻姑（装备打造）处打造。</font>"."<br>";
+echo "<font color=black>（5）普陀山：【伏虎套装】在普陀山-惠岸行者（装备打造）处打造。</font>"."<br>";
+echo "<font color=black>（6）方寸山：【七星套装】在方寸山-老道士（装备打造）处打造。</font>"."<br>";
 echo "<font color=black>*装备升级方法：状态-装备-相应要升级的装备-装备升级-按提示准备相应的材料升级-确定升级。</font>"."<br>";
-echo "<font color=red>PS：以上攻略来自玩家：LoKC（10000032）提供</font>"."<br>";
+echo "<br>";
+
+echo "<font color=red>1.人物装备升级表</font>"."<br>";
+echo "<font color=black>师门套装-待添加攻略</font>"."<br>";
+echo "<font color=black>50级【幻想套装】</font>"."<br>";
+echo "<font color=black>99级【幻想套（1级）】</font>"."<br>";
+echo "<font color=black>109级【幻想套（2级）】</font>"."<br>";
+echo "<font color=black>139级【幻想套（3级）】</font>"."<br>";
+echo "<font color=black>159级【诛仙套】</font>"."<br>";
+echo "<font color=black>179级【圣灵套】【圣灵套（1级）】【圣灵套（2级）】【圣灵套（3级）】【圣灵☯天元套】</font>"."<br>";
+echo "<font color=black>199级【噬魂套】</font>"."<br>";
+echo "<font color=black>200级【帝王套】</font>"."<br>";
+echo "<font color=black>220级【仙尊套】</font>"."<br>";
+echo "<font color=black>230级【神尊套】</font>"."<br>";
+echo "<font color=black>240级【圣尊套】</font>"."<br>";
+echo "<font color=black>250级【混沌天尊套】</font>"."<br>";
+echo "<font color=black>260级【混沌神尊套】</font>"."<br>";
+echo "<font color=black>270级【混沌圣尊套】</font>"."<br>";
+echo "<font color=black>280级【鸿蒙天尊套】</font>"."<br>";
+echo "<font color=black>290级【鸿蒙神尊套】</font>"."<br>";
+echo "<font color=black>300级【鸿蒙圣尊套】</font>"."<br>";
+echo "<br>";
+
+echo "<font color=red>2.人物时装升级表</font>"."<br>";
+echo "<font color=black>【小恶魔の套装】（1级时装）</font>"."<br>";
+echo "<font color=black>【小天使の套装】（2级时装）</font>"."<br>";
+echo "<font color=black>【黑骑士の套装】（3级时装）</font>"."<br>";
+echo "<font color=black>【猎天使の套装】（4级时装）</font>"."<br>";
+echo "<font color=black>【死神の套装】（5级时装）</font>"."<br>";
+echo "<br>";
+
+echo "<font color=red>3.人物婚饰升级表</font>"."<br>";
+echo "<font color=black>40级【心心相印】（婚戒）【心心相印】（婚链）</font>"."<br>";
+echo "<font color=black>99级【心有灵犀】（婚戒）【心有灵犀】（婚链）</font>"."<br>";
+echo "<font color=black>139级【山盟海誓】（婚戒）【山盟海誓】（婚链）</font>"."<br>";
+echo "<font color=black>159级【海枯石烂】（婚戒）【海枯石烂】（婚链）</font>"."<br>";
+echo "<font color=black>179级【天作之合】（婚戒）【天作之合】（婚链）</font>"."<br>";
+echo "<font color=black>199级【天赐良缘の婚戒】【天赐良缘の婚链】</font>"."<br>";
+echo "<font color=black>200级【天仙之恋の婚戒】【天仙之恋の婚链】</font>"."<br>";
+echo "<br>";
+
+echo "<font color=red>4.人物典藏婚饰升级表</font>"."<br>";
+echo "<font color=black>139级【比翼双飞の婚戒】（典藏版）【比翼双飞の婚链】（典藏版）</font>"."<br>";
+echo "<font color=black>179级【一生所爱の婚戒】（典藏版）【一生所爱の婚链】（典藏版）</font>"."<br>";
+echo "<font color=black>199级【缘定今生の婚戒】（典藏版）【缘定今生の婚链】（典藏版）</font>"."<br>";
+echo "<font color=black>200级【天尊之恋の婚戒】（典藏版）【天尊之恋の婚链】（典藏版）</font>"."<br>";
+echo "<br>";
+
+echo "<font color=red>5.人物法宝升级表</font>"."<br>";
+echo "<font color=black>99级【阐气瓶】（1级-5级）</font>"."<br>";
+echo "<font color=black>99级【开天珠】（1级-5级）</font>"."<br>";
+echo "<font color=black>99级【定海珠】（1级-5级）</font>"."<br>";
+echo "<font color=black>109级【乾坤圈】（1级-5级）</font>"."<br>";
+echo "<font color=black>139级【金箍棒】（1-8级）</font>"."<br>";
+echo "<font color=black>159级【金箍棒】（9-20级）</font>"."<br>";
+echo "<font color=black>199级【如意金箍棒】（1-8级）</font>"."<br>";
+echo "<font color=black>200级【如意金箍棒】（9-20级）</font>"."<br>";
+echo "<br>";
+
+echo "<font color=red>6.人物坐骑升级表</font>"."<br>";
+echo "<font color=black>139级【镇东神兽】（青龙）</font>"."<br>";
+echo "<font color=black>159级【镇东神兽】青龙☯仙</font>"."<br>";
+echo "<font color=black>179级【筋斗云】☯（血）</font>"."<br>";
+echo "<font color=black>199级【镇东神兽】青龙☯至尊</font>"."<br>";
+echo "<font color=black>200级【天尊神兽】（青龙）</font>"."<br>";
+echo "<br>";
+
+echo "<font color=black>139级【镇东神兽】（白虎）</font>"."<br>";
+echo "<font color=black>159级【镇东神兽】白虎☯仙</font>"."<br>";
+echo "<font color=black>179级【筋斗云】☯（攻）</font>"."<br>";
+echo "<font color=black>199级【镇东神兽】白虎☯至尊</font>"."<br>";
+echo "<font color=black>200级【天尊神兽】（白虎）</font>"."<br>";
+echo "<br>";
+
+echo "<font color=black>139级【镇东神兽】（朱雀）</font>"."<br>";
+echo "<font color=black>159级【镇东神兽】朱雀☯仙</font>"."<br>";
+echo "<font color=black>179级【筋斗云】☯（魔攻）</font>"."<br>";
+echo "<font color=black>199级【镇东神兽】朱雀☯至尊</font>"."<br>";
+echo "<font color=black>200级【天尊神兽】（朱雀）</font>"."<br>";
+echo "<br>";
+
+echo "<font color=black>139级【镇东神兽】（玄武）</font>"."<br>";
+echo "<font color=black>159级【镇东神兽】玄武☯仙</font>"."<br>";
+echo "<font color=black>179级【筋斗云】☯（防御）</font>"."<br>";
+echo "<font color=black>199级【镇东神兽】玄武☯至尊</font>"."<br>";
+echo "<font color=black>200级【天尊神兽】（玄武）</font>"."<br>";
+echo "<br>";
+
+echo "<font color=red>7.宠物装备升级表</font>"."<br>";
+echo "<font color=black>1级〖小兽套〗</font>"."<br>";
+echo "<font color=black>60级〖大兽套〗</font>"."<br>";
+echo "<font color=black>100级〖顽兽套〗</font>"."<br>";
+echo "<font color=black>120级〖猛兽套〗</font>"."<br>";
+echo "<font color=black>150级〖神兽兽套〗</font>"."<br>";
+echo "<font color=black>180级〖圣兽兽套〗</font>"."<br>";
+echo "<font color=black>200级〖玛雅兽套〗</font>"."<br>";
+echo "<br>";
+echo "<br>";
 
 
 //cmd及超链接值
@@ -29,7 +124,3 @@ echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>返回游�
 echo "<font color=black>----------------------</font>"."<br>";
 //cmd及超链接值
 include("fhgame.php");
-
-
-
-?>
