a5a2ee8f23c34a1e5621407b7c4def867be21734
kkajy0621 <811051558@qq.com>
2022-09-30 01:09:23 +0800

    更新升级攻略

--- fqxy/template/xy405.php
+++ fqxy/template/xy405.php
@@ -1,20 +1,20 @@
 <?php
 
+echo "<font color=red>【幻想西游升级攻略】</font>"."<br>";
+echo "<font color=biue>【新手升级攻略】</font>"."<br>";
+echo "<font color=black>打开村长赠送的【新手礼包】，里面有你所需的初级装备和药品。换上装备。就是时候开启你的西游征程了。</font>"."<br>";
+echo "<font color=black>（1-5级）完成好村长交给你的任务，拿到船票前往南城客栈找店小二。</font>"."<br>";
+echo "<font color=black>（5-50级）跟着主线任务一直走，记得把新手礼包送的蟠桃吃了升级更快速</font>"."<br>";
+echo "<font color=black>（60-300级）</font>"."<br>";
+echo "<font color=black>玄武大街-VIP如意传送员传送进VIP练级场挂机升级</font>"."<br>";
+echo "<font color=black>白虎大街做每日任务</font>"."<br>";
+echo "<font color=black>挑战各大副本可获得高额银两道具奖励跟经验</font>"."<br>";
+echo "<font color=blue>【老玩家升级攻略】</font>"."<br>";
+echo "<font color=black>白虎大街挑战伏羲阵可获得高额经验</font>"."<br>";
+echo "<font color=black>福利大厅-商城购买财宝箱获得银两直接去许愿池用许愿可获得高额经验</font>"."<br>";
+echo "<br>";
+echo "<br>";
 
-
-
-
-echo "<font color=black>大唐年间,妖魔四起,无数英雄侠女立志救黎民百姓于水生火热当中！英雄侠女们历劫四方，幸得无名仙人指点迷津……</font>"."<br>";
-echo "<font color=black>初踏西游路，切记先打开村长赠送的【新手礼包】，里面有你所需的初级装备和药品。换上装备，就是时候开启你的西游征程了。</font>"."<br>";
-echo "<font color=black>（1-5级）完成好村长交给你的任务，拿到船票，你就可以出发前往更繁华、更热闹的长安城。</font>"."<br>";
-echo "<font color=black>（5-30级）初到长安，人生路不熟，还是要乖乖的听南城客栈店小二的吩咐，完成主线任务噢！</font>"."<br>";
-echo "<font color=black>（30-40级）狐妖和酒鬼固然厉害，但是为了清清和晓月，你可要升级装备、提升等级，提高自己的实力来击败他们。村长的【新手礼包】里有上好的蟠桃，不妨一尝~</font>"."<br>";
-echo "<font color=black>（40-60级）热闹的长安流传着很多的说法。听闻白虎大街的大唐国师十分赏识骁勇之士…玄武大街如意传送员处有十全大补的唐小僧肉……了解一下？</font>"."<br>";
-echo "<font color=black>（60-80级）传说中，有不少的地方已被妖魔占领。冰晶塔、变异竹林…一个个骇人听闻的名字…正等待着你前往斩妖除魔。带着副本boss的首级去师门换取装备吧！</font>"."<br>";
-echo "<font color=black>到这里，相信你已经有足够的实力在西游路上披荆斩棘，剩下的就全靠你的觉悟能力了。</font>"."<br>";
-echo "<font color=black>西游路上尽坎坷，重重难关我愈勇！</font>"."<br>";
-echo "<font color=black>PS：别忘了高家庄还有一个可恶的酒鬼！</font>"."<br>";
-echo "<font color=red>注：以上攻略来自玩家：LoKC（10000032）提供</font>"."<br>";
 //cmd及超链接值
 $cmid=$cmid+1;
 $cdid[]=$cmid;
@@ -24,7 +24,3 @@ echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>返回游�
 echo "<font color=black>----------------------</font>"."<br>";
 //cmd及超链接值
 include("fhgame.php");
-
-
-
-?>
