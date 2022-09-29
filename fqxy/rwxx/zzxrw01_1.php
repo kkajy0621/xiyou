1661467dc7b288c46a6d2ced52ff8d29e8aeb8f1
kkajy0621 <811051558@qq.com>
2022-09-29 21:07:12 +0800

    完善全等级转职任务在任务分类查看无报错

--- fqxy/rwxx/zzxrw01_1.php
+++ fqxy/rwxx/zzxrw01_1.php
@@ -1,48 +1,23 @@
 <?php
-
 if ($rid==2 || $rid == 1) {
-    echo "<font color=black>李白：小仙卿，老夫看你仙风道骨，背后有一道淡淡的仙气，理应在天上处理天庭杂务，莫非是天仙下凡，体察民间疾苦？</font>"."<br>";
-    echo "<font color=black>".$name."：啊？李白老夫子，我只是一介武夫，并不是神仙啊！</font>"."<br>";
-    echo "<font color=black>提示：（满159级找李白转职）</font>"."<br>";
+echo "<font color=black>李白：小仙卿，老夫看你仙风道骨，背后有一道淡淡的仙气，理应在天上处理天庭杂务，莫非是天仙下凡，体察民间疾苦？</font>"."<br>";
+echo "<font color=black>".$name."：啊？李白老夫子，我只是一介武夫，并不是神仙啊！</font>"."<br>";
+echo "<font color=black>提示：（满159级找李白转职）</font>"."<br>";
 
 } elseif ($rid==3) {
-
 echo "<font color=black>李白：不应该不应该，你快去师门祖师面前问问，是不是忘了把你列入仙班了！</font>"."<br>";
 
-
-
 } elseif ($rid==4) {
-
 echo "<font color=black>".$name."：弟子拜见祖师爷！</font>"."<br>";
-echo "<font color=black>师门祖师：噢？小轩GM你来了，所为何事？</font>"."<br>";
-
-
-
-
-
-
+echo "<font color=black>师门祖师：噢？".$name."你来了，所为何事？</font>"."<br>";
 
 } elseif ($rid==5) {
 echo "<font color=black>".$name."：弟子初到长安之时，幸得师门收留，传授除魔斩妖的道理。弟子谨记师门教诲，外出历劫四方，拯救无数黎明百姓，论修行，弟子是否应该……应该……位列仙班？</font>"."<br>";
 echo "<font color=black>师门祖师：呵呵，原来为的此事。实不相瞒，如今阐教和截教再为封神之事的而纷争不绝，封神一事迟迟未决。你去天宫一躺了解下情况，看能不能调停封神之争，帮助能人异士顺利成仙。</font>"."<br>";
 
-
-
-
-
-
-
-
 } elseif ($rid==6) {
-echo "<font color=black>接引仙子：小仙卿前来天宫，可是为封神一事？</font>"."<br>";	
+echo "<font color=black>接引仙子：小仙卿前来天宫，可是为封神一事？</font>"."<br>";
 echo "<font color=black>".$name."：仙人果然神机妙算。不知仙人可否详尽道来封神大战一事？</font>"."<br>";
-
-
-
-
-
-
-
 
 } elseif ($rid==7) {
 echo "<font color=black>接引仙子：（介绍阐教，截教）。阐教李天王在天王殿，截教李公明在通明宫，小仙卿可以加入其中一教，便可得道成仙。</font>"."<br>";
@@ -51,44 +26,27 @@ echo "<font color=black>接引仙子：（介绍阐教，截教）。阐教李�
 echo "<font color=black>接引仙子：小仙卿骨骼精奇，可否愿意助我教完成封神大业，到时定会为小仙卿留一仙席。</font>"."<br>";
 echo "<font color=black>".$name."：愿为效劳！</font>"."<br>";
 
-
 } elseif ($rid==9) {
 echo "<font color=black>接引仙子：如今凡间群妖四起，小仙卿去平定各方妖怪（击杀通天塔10层终极BOSS）</font>"."<br>";
+
 } elseif ($rid==10) {
 echo "<font color=black>接引仙子：如今凡间群妖四起，小仙卿去平定各方妖怪（已击杀）</font>"."<br>";
-
-
 
 } elseif ($rid==11) {
 echo "<font color=black>接引仙子：不错不错。开封广场内通天塔内妖气冲天，其中有一妖怪幻化成【圣婴大王】，更是到处诋毁我教，小仙卿去教训一下它。（击败通天塔15层【圣婴大王】×1）</font>"."<br>";
 
-
-
-
 } elseif ($rid==12) {
 echo "<font color=black>接引仙子：不错不错。开封广场内通天塔内妖气冲天，其中有一妖怪幻化成【圣婴大王】，更是到处诋毁我教，小仙卿去教训一下它。（击败通天塔15层【圣婴大王】×1）（已完成）</font>"."<br>";
-
-
-
-
 
 } elseif ($rid==13) {
 echo "<font color=black>接引仙子：你回来的正是时候，《封神榜》自上次阐截大战后便化成碎片，刚才有消息传来其中一块碎片落入了昆仑山区烈炽灵狐手中，你速速前去取回。</font>"."<br>";
 
-
-
 } elseif ($rid==14) {
 echo "<font color=black>烈炽灵狐：大胆凡人竟敢闯入我火焰山！</font>"."<br>";
-	
 echo "<font color=black>".$name."：我是奉？教之命前来取回封神榜碎片，快快交出方可饶你一命！</font>"."<br>";
 
-
-
 } elseif ($rid==15||$rid==16) {
-
 echo "<font color=black>烈炽灵狐：少废话，看我九味真火！（击败烈炽灵狐）</font>"."<br>";
-
-
 
 } elseif ($rid==17||$rid==18||$rid==19) {
 echo "<font color=black>烈炽灵狐：果然是仙界中人！我妖界亦非等闲之辈！看我分身术！（击败烈炽灵狐分身）</font>"."<br>";
@@ -96,28 +54,20 @@ echo "<font color=black>烈炽灵狐：果然是仙界中人！我妖界亦非�
 } elseif ($rid==20||$rid==21) {
 echo "<font color=black>烈炽灵狐：果然是仙界中人！我妖界亦非等闲之辈！看我分身术！（击败烈炽灵狐真身）</font>"."<br>";
 
-
 } elseif ($rid==22) {
 echo "<font color=black>烈炽灵狐：只见烈炽灵狐在地上奄奄一息，趁你不注意的时候在一溜青烟中消失了。</font>"."<br>";
 
-
 } elseif ($rid==23) {
-echo "<font color=black>接引仙子：小仙卿果然功夫了得。但是接下来的任务可不是考功夫那么简单哦！方寸山瑶台的菩提祖师是鸿钧老祖大弟子的化身，他手中也有一块封神榜碎片，只赠与有缘人。</font>"."<br>"; 		
+echo "<font color=black>接引仙子：小仙卿果然功夫了得。但是接下来的任务可不是考功夫那么简单哦！方寸山瑶台的菩提祖师是鸿钧老祖大弟子的化身，他手中也有一块封神榜碎片，只赠与有缘人。</font>"."<br>";
 
 } elseif ($rid==24) {
-echo "<font color=black>方寸山瑶台的菩提祖师偷取锦囊</font>"."<br>"; 		
+echo "<font color=black>方寸山瑶台的菩提祖师偷取锦囊</font>"."<br>";
 
 } elseif ($rid==25) {
-
-echo "<font color=black>菩提祖师(转职):恭喜你！！你已完成了159转职任务（达到199玩家可前往方寸山瑶台进行转职）</font>"."<br>"; 		
-
+echo "<font color=black>菩提祖师(转职):恭喜你！！你已完成了159转职任务（达到199玩家可前往方寸山瑶台进行转职）</font>"."<br>";
 
 } elseif ($rid==26) {
-
-
-echo "<font color=black>菩提祖师(转职):$name</font>"."<font color=black>你可总算回来了！你西游的这段时间，大唐皇帝李世民竟不念我门曾经为大唐除尽妖魔，公然与我门为敌，大肆剿杀我门弟子。从现在起我门与大唐势不两立，你快去取下大唐皇帝李世民的首级！</font>"."<br>";
-
-
+echo "<font color=black>菩提祖师(转职):一个过客你可总算回来了！你西游的这段时间，大唐皇帝李世民竟不念我门曾经为大唐除尽妖魔，公然与我门为敌，大肆剿杀我门弟子。从现在起我门与大唐势不两立，你快去取下大唐皇帝李世民的首级！</font>"."<br>";
 
 } elseif ($rid==27) {
 echo "<font color=black>请到方寸山瑶台菩提祖师(转职)处复命（大唐皇帝已击杀）</font>"."<br>";
@@ -126,99 +76,162 @@ echo "<font color=black>请到方寸山瑶台菩提祖师(转职)处复命（大
 echo "<font color=black>".$nname."：哈~哈~哈~干得不错！（只见师门祖师将李世民的灵窍吸入口中，慈祥的面孔瞬间变得狰狞。）天子自有灵气护体，果然只有凡人才能接近李世民。李世民，我夫君有救了。该死的还有".$name."你！</font>"."<br>";
 echo "<font color=black>".$name."：师门祖师，你……</font>"."<br>";
 
+} elseif ($rid==29) {
+echo "<font color=black>菩提祖师：你睁大眼睛看看我是谁！（师门祖师变成了一只人首蛇身的女妖怪）。</font>"."<br>";
+echo "<font color=black>".$name."：原来是万年蛇妖！你竟敢冒充我祖师爷，骗我杀掉大唐圣君，今天我可饶不了你！</font>"."<br>";
+
+} elseif ($rid==30) {
+echo "<font color=black>方寸山-瑶台击败万年蛇妖【已击杀】</font>"."<br>";
+
+
+} elseif ($rid==31) {
+echo "<font color=black>可恶，居然让万年蛇妖给逃跑了，我去天宫问个究竟。</font>"."<br>";
+echo "<font color=black>".$name."：看来得去天宫太上老君处查明情况</font>"."<br>";
+
+} elseif ($rid==32) {
+echo "<font color=black>太上老君：你这个小顽童又来了，最近我没炼丹药，不要问我拿丹药了……</font>"."<br>";
+echo "<font color=black>".$name."：太上老君，我这次不是为了丹药而来，而是……（你向太上老君道出了事情的经过）。</font>"."<br>";
+
+} elseif ($rid==33) {
+echo "<font color=black>袁天罡：别来无恙啊！来，我们去喝杯酒叙叙旧……</font>"."<br>";
+echo "<font color=black>".$name."：天罡兄且慢，叙旧之事可以他日再谈，如今有一急事想求得照妖镜。</font>"."<br>";
+
+} elseif ($rid==34) {
+echo "<font color=black>袁天罡：哎呀，照妖镜不巧被我打碎了，你去收集1000个〖百年魔珠〗，我帮你马上炼制一个。</font>"."<br>";
+
+} elseif ($rid==35) {
+echo "<font color=black>袁天罡：哎呀，照妖镜不巧被我打碎了，你去收集1000个〖百年魔珠〗，我帮你马上炼制一个。【已完成】</font>"."<br>";
+
+} elseif ($rid==36) {
+echo "<font color=black>袁天罡：啊？要让万年蛇妖现形，这普通的照妖镜还不够法力，你再去收集100个〖陨魂〗给我吧。</font>"."<br>";
+
+} elseif ($rid==37) {
+echo "<font color=black>袁天罡：稍等一下，马上完成。来，拿好这块宝镜。</font>"."<br>";
+
+} elseif ($rid==38) {
+echo "<font color=black>".$name."：你放下了照妖镜，不一会儿，万年蛇妖显出了真身。</font>"."<br>";
+echo "<font color=black>万年蛇妖：噢？你自己找上门来了！那正好让你尝尝死亡的滋味，也好为我夫君报仇！（击败万年蛇妖）</font>"."<br>";
+
+} elseif ($rid==39) {
+echo "<font color=black>请到长安城天监台袁天罡处复命（万年蛇妖已击杀）</font>"."<br>";
+
+} elseif ($rid==40) {
+echo "<font color=black>万年蛇妖：倒在地上的蛇妖瞬间复活，张开大口两只大毒牙向你咬去，你被咬死了！（传送至阴曹地府）
+你被蛇妖咬死了~~~~~</font>"."<br>";
+
+} elseif ($rid==41) {
+echo "<font color=black>阎罗王：丁酉年甲午月乙卯日被万年蛇妖所杀，下世轮回投胎为猪……</font>"."<br>";
+echo "<font color=black>".$name."：等等……我还有要务在身，不能那么早死……</font>"."<br>";
+
+} elseif ($rid==42) {
+echo "<font color=black>阎罗王：阎王要你三更死，岂可留你到五更，黑白无常，上！</font>"."<br>";
+echo "<font color=black>".$name."：岂有此理，是非不分，今天我非大闹阎罗殿不可！（击败黑无常、白无常）</font>"."<br>";
+
+} elseif ($rid==43) {
+echo "<font color=black>提示；地府-阎罗殿（击败白无常）【已完成】</font>"."<br>";
+
+} elseif ($rid==44) {
+echo "<font color=black>提示；地府-阎罗殿（击败黑无常）【已完成】</font>"."<br>";
+
+} elseif ($rid==45) {
+echo "<font color=black>阎罗王：小子本王饶你一命（跑去阴曹地府了）（阴曹地府20层）</font>"."<br>";
+echo "<font color=black>".$name."：哪里跑~~~快给我还阳啊</font>"."<br>";
 
 } elseif ($rid==46) {
-    echo "<font color=black>阴曹地府20层击败【阎罗王】</font>"."<br>";
-    echo "<font color=black>回地府寻找阎罗王</font>"."<br>";
+echo "<font color=black>阴曹地府20层击败【阎罗王】【已完成】</font>"."<br>";
+
+} elseif ($rid==47) {
+echo "<font color=black>阎罗王：好啦~~本王也是明事理的人，我这就帮你还阳</font>"."<br>";
+echo "<font color=black>".$name."：算你识相</font>"."<br>";
 
 } elseif ($rid==48) {
-    $nname = '太上老君';
-    echo "<font color=black>".$nname."：李世民的灵魂在阴曹地府 【菩提老祖】三十层BOSS手中，你去把他的灵魂带回来。</font>"."<br>";
-    echo "<font color=black>提示：（击败阴曹地府 【菩提老祖】三十层BOSS）</font>"."<br>";
+echo "<font color=black>太上老君：李世民的灵魂在阴曹地府 【菩提老祖】三十层BOSS手中，你去把他的灵魂带回来。</font>"."<br>";
+echo "<font color=black>提示：（击败阴曹地府 【菩提老祖】三十层BOSS）</font>"."<br>";
+
 } elseif ($rid==49) {
-    $nname = '太上老君';
-    echo "<font color=black>".$nname."：李世民的灵魂在阴曹地府 【菩提老祖】三十层BOSS手中，你去把他的灵魂带回来。</font>"."<br>";
-    echo "<font color=black>提示：（天宫-{$nname}）</font>"."<br>";
+echo "<font color=black>太上老君：李世民的灵魂在阴曹地府 【菩提老祖】三十层BOSS手中，你去把他的灵魂带回来。</font>"."<br>";
+echo "<font color=black>提示：（击败阴曹地府 【菩提老祖】三十层BOSS）【已完成】</font>"."<br>";
+
+} elseif ($rid==50) {
+$nname = '太上老君';
+echo "<font color=black>".$nname."：好，我这就开坛做法。（只见太上老君口中念着咒语，李世民的灵窍和灵魂消失了）小仙卿，如今李世民已还阳，你可以放心离去了。</font>"."<br>";
+echo "<font color=black>".$name."：那……那……那我还没还阳啊！</font>"."<br>";
+echo "<font color=black>".$nname."：还阳术乃仙家禁书，为李世民还阳已消耗我大半法力，暂时无力为你还魂。你的师门祖师已云游归来，你去看看有什么办法吧。</font>"."<br>";
+echo "<font color=black>npc提示：（方寸山-瑶台-菩提祖师）</font>"."<br>";
+
+} elseif ($rid==51) {
+$nname = '菩提祖师';
+echo "<font color=black>".$name."：祖师你回来的正好，弟子我……</font>"."<br>";
+echo "<font color=black>".$nname."：嘘~为师已经了解一切。家丑不可外扬，这里有颗【九转仙丹】，你吃下便可还阳。</font>"."<br>";
+
+} elseif ($rid==52) {
+$nname = '菩提祖师';
+echo "<font color=black>".$nname."：大事不妙了大仙！</font>"."<br>";
+echo "<font color=black>".$name."：又怎么了？</font>"."<br>";
+echo "<font color=black>".$nname."：最近长安出现了一个叫“伏羲阵”的妖阵，很多百姓进去以后就疯疯癫癫，有的突发大病，久治不愈，他们就在回春药铺，大仙你法力高强，你快去救救他们吧。</font>"."<br>";
+
+} elseif ($rid==53) {
+$nname = '杨中顺';
+echo "<font color=black>".$nname."：大仙你来的正好，这几位病人的症状分明是中了瘟疫，但是任凭怎么给他们喂服治疗瘟疫的药也是不能痊愈，真是奇怪。</font>"."<br>";
+echo "<font color=black>".$name."：你走近一看，发现这些病人手臂上都有一个淡淡的仙桃状黑印。你心想，袁天罡见多识广，于是前往天监台</font>"."<br>";
+
+} elseif ($rid==54) {
+$nname = '袁天罡';
+echo "<font color=black>".$nname."：你又来了，这次又遇到什么麻烦事了？（你向袁天罡描述了事情的经过）</font>"."<br>";
+echo "<font color=black>".$nname."：伏羲阵？仙桃印？莫非…莫非是……莫非是瑶池圣母冲破了盘古封印，要用“伏羲阵”来毁灭人间？</font>"."<br>";
+echo "<font color=black>".$name."：瑶池圣母？</font>"."<br>";
 
 } elseif ($rid==55) {
-    $nname = '杨中顺';
-    echo "<font color=black>".$nname."：大仙你来的正好，这几位病人的症状分明是中了瘟疫，但是任凭怎么给他们喂服治疗瘟疫的药也是不能痊愈，真是奇怪。</font>"."<br>";
-    echo "<font color=black>".$name."：你走近一看，发现这些病人手臂上都有一个淡淡的仙桃状黑印。你心想，袁天罡见多识广，于是前往天监台</font>"."<br>";
-    $ydx=1;
-    $ydy=23;
-    $ydfy=5000000;
-    include(XY_DIR . "/rw/ksrw.php");
+$nname = '袁天罡';
+echo "<font color=black>".$nname."：对。相传盘古初开，女娲造人，人王伏羲作为人中之王，他十分疼爱自己的子民。有一天，人王伏羲遇上了掌管人间刑罚、散布瘟疫的瑶池圣母，两人一见钟情。岂料人王伏羲最后还是知道了瑶池圣母是上天派下来惩罚人类的女神，两人最终反目成仇，大打出手。在筋疲力尽之际，人王伏羲用最后的力量使出盘古封印将瑶池圣母封印起来，而他自己也自愿堕入轮回，以洗脱罪孽……这样，你去收集100个【黑晶】、100个【灵晶】给我制作幻灵咒，看看【瑶池】（副本）内是否有异样。
+你去收集100个【黑晶】、100个【灵晶】</font>"."<br>";
+
+} elseif ($rid==56) {
+$nname = '袁天罡';
+echo "<font color=black>".$nname."：对。相传盘古初开，女娲造人，人王伏羲作为人中之王，他十分疼爱自己的子民。有一天，人王伏羲遇上了掌管人间刑罚、散布瘟疫的瑶池圣母，两人一见钟情。岂料人王伏羲最后还是知道了瑶池圣母是上天派下来惩罚人类的女神，两人最终反目成仇，大打出手。在筋疲力尽之际，人王伏羲用最后的力量使出盘古封印将瑶池圣母封印起来，而他自己也自愿堕入轮回，以洗脱罪孽……这样，你去收集100个【黑晶】、100个【灵晶】给我制作幻灵咒，看看【瑶池】（副本）内是否有异样。
+你去收集100个【黑晶】、100个【灵晶】【已完成】</font>"."<br>";
+
 } elseif ($rid==57) {
-    $nname = '袁天罡';
-    echo "<font color=black>".$nname."：盘古封印果然被冲破，大仙你立刻去【瑶池】（副本）铲除这棵蟠桃树！（传送至【瑶池】（副本），击败蟠桃树）</font>"."<br>";
-    $ydx=23;//移动坐标x
-    $ydy=23;//移动坐标y
-    $ydfy=5000000;//传送费用
-    include("./rw/ksrw.php");
+$nname = '袁天罡';
+echo "<font color=black>".$nname."：盘古封印果然被冲破，大仙你立刻去【瑶池】（副本）铲除这棵蟠桃树！（传送至【瑶池】（副本），击败蟠桃树）</font>"."<br>";
 
 } elseif ($rid==58) {
-    $nname = '瑶池圣母';
-    echo "<font color=black>".$nname."：大胆妖孽竟敢拔我蟠桃树，拿命来！（瑶池圣母一掌将你击倒在地上。幸好你有仙气护体，免于一死）</font>"."<br>";
-    $ydx=23;//移动坐标x
-    $ydy=0;//移动坐标y
-    $ydfy=5000000;//传送费用
-    include("./rw/ksrw.php");
+$nname = '袁天罡';
+echo "<font color=black>".$nname."：盘古封印果然被冲破，大仙你立刻去【瑶池】（副本）铲除这棵蟠桃树！（传送至【瑶池】（副本），击败蟠桃树）【已完成】</font>"."<br>";
 
 } elseif ($rid==59) {
-    $nname = '瑶池圣母';
-    echo "<font color=black>".$nname."：大胆妖孽竟敢拔我蟠桃树，拿命来！（瑶池圣母一掌将你击倒在地上。幸好你有仙气护体，免于一死）</font>"."<br>";
-    $ydx=23;//移动坐标x
-    $ydy=0;//移动坐标y
-    $ydfy=5000000;//传送费用
-    include("./rw/ksrw.php");
-
+$nname = '瑶池圣母';
+echo "<font color=black>".$nname."：大胆妖孽竟敢拔我蟠桃树，拿命来！（瑶池圣母一掌将你击倒在地上。幸好你有仙气护体，免于一死）</font>"."<br>";
 
 } elseif ($rid==60) {
-    $nname = '接引仙子';
-    echo "<font color=black>".$nname."：完了完了，瑶池圣母重现，欺骗玉帝吃下了有毒的蟠桃，并扬言要灭世。现在玉帝被邪灵入侵，仙灵被带到了【通天塔】30层，你快去救他。（击败【通天塔】30层怪物）</font>"."<br>";
-    //$ydx=78;//移动坐标x
-    //$ydy=59;//移动坐标y
-    //$ydfy=100000;//传送费用
-    //include("./rw/ksrw.php");
+$nname = '接引仙子';
+echo "<font color=black>".$nname."：完了完了，瑶池圣母重现，欺骗玉帝吃下了有毒的蟠桃，并扬言要灭世。现在玉帝被邪灵入侵，仙灵被带到了【通天塔】30层，你快去救他。（击败【通天塔】30层怪物）</font>"."<br>";
 
 } elseif ($rid==61) {
-    $nname = '接引仙子';
-    echo "<font color=black>".$nname."：完了完了，瑶池圣母重现，欺骗玉帝吃下了有毒的蟠桃，并扬言要灭世。现在玉帝被邪灵入侵，仙灵被带到了【通天塔】30层，你快去救他。（击败【通天塔】30层怪物）</font>"."<br>";
-    echo "<font color=black>提示：（天宫-{$nname}）</font>"."<br>";
-    $ydx=23;//移动坐标x
-    $ydy=0;//移动坐标y
-    $ydfy=100000;//传送费用
-    include("./rw/ksrw.php");
+$nname = '接引仙子';
+echo "<font color=black>".$nname."：完了完了，瑶池圣母重现，欺骗玉帝吃下了有毒的蟠桃，并扬言要灭世。现在玉帝被邪灵入侵，仙灵被带到了【通天塔】30层，你快去救他。（击败【通天塔】30层怪物）【已完成】</font>"."<br>";
 
-} elseif ($rid==62 || $rid == 63) {
-    $nname = '接引仙子';
-    echo "<font color=black>".$nname."：玉帝告诉了你盘古封印的秘密，并命你去“伏羲阵”处击败【如来佛祖】x10。（玩家击败【如来佛祖】x10）</font>"."<br>";
-    $ydx=1;//移动坐标x
-    $ydy=10;//移动坐标y
-    $ydfy=100000;//传送费用
-    include("./rw/ksrw.php");
+} elseif ($rid==62) {
+$nname = '接引仙子';
+echo "<font color=black>".$nname."：玉帝告诉了你盘古封印的秘密，并命你去“伏羲阵”处击败【如来佛祖】x10。（玩家击败【如来佛祖】x10）</font>"."<br>";
+
+} elseif ($rid==63) {
+$nname = '接引仙子';
+echo "<font color=black>".$nname."：玉帝告诉了你盘古封印的秘密，并命你去“伏羲阵”处击败【如来佛祖】x10。（玩家击败【如来佛祖】x10）</font>"."<br>";
 
 } elseif ($rid==64) {
-    $nname = '接引仙子';
-    echo "<font color=black>".$nname."：玉帝告诉了你盘古封印的秘密，并命你去“伏羲阵”处击败【如来佛祖】x10。（玩家击败【如来佛祖】x10）</font>"."<br>";
-    echo "<font color=black>提示：（天宫-{$nname}）</font>"."<br>";
-    $ydx=23;//移动坐标x
-    $ydy=0;//移动坐标y
-    $ydfy=100000;//传送费用
-    include("./rw/ksrw.php");
+$nname = '接引仙子';
+echo "<font color=black>".$nname."：玉帝告诉了你盘古封印的秘密，并命你去“伏羲阵”处击败【如来佛祖】x10。（玩家击败【如来佛祖】x10）【已完成】</font>"."<br>";
 
 } elseif ($rid==65) {
-    $nname = '接引仙子';
-    echo "<font color=black>".$nname."：修补成功：只见立刻风气云涌，大地间响起了【盘古族咒】，瑶池圣母痛不欲生，请立即前往菩提师祖处领取仙20解封丹</font>"."<br>";
+$nname = '接引仙子';
+echo "<font color=black>".$nname."：修补成功：只见立刻风气云涌，大地间响起了【盘古族咒】，瑶池圣母痛不欲生，请立即前往菩提师祖处领取仙20解封丹</font>"."<br>";
 
 } elseif ($rid==66) {
-    echo "<font color=black>菩提祖师(转职):这里赠与你一颗【伏羲仙丹】祝你以后西游路上劈荆斩刺</font>"."<br>";
+$nname = '菩提祖师(转职)';
+echo "<font color=black>".$nname."：这里赠与你一颗【伏羲仙丹】祝你以后西游路上劈荆斩刺</font>"."<br>";
 
 } else{
 echo "<font color=black>没有这个任务变量：".$rid."请尝试联系gm解决此问题！！</font><br>";
 }
-
-
-
-
 ?>
