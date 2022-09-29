58b2a74adf31fe724dedcd635f118d63d61559db
kkajy0621 <811051558@qq.com>
2022-09-30 00:05:08 +0800

    SDK码兑换转移到系统

--- fqxy/template/xy203.php
+++ fqxy/template/xy203.php
@@ -4,48 +4,45 @@
 //阻塞代码防止出现脏数据
 $ininalock=$wjid."_lock".".txt";
 include("./ini/zsini.php");
-if($zsspd==1){
+if ($zsspd==1) {
+    echo "<font color=black>【页面设置】</font><br>";
 
+    //cmd及超链接值
+    $cmid=$cmid+1;
+    $cdid[]=$cmid;
+    $clj[]=204;
+    $npc[]=0;
+    echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>显示设置</font></a>";
+    echo "<font color=black>-</font>";
 
+    //cmd及超链接值
+    $cmid=$cmid+1;
+    $cdid[]=$cmid;
+    $clj[]=305;
+    $npc[]=0;
+    echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>还原快捷设置</font></a>";
 
-echo "<font color=black>【页面设置】</font><br>";
-
-//cmd及超链接值
-$cmid=$cmid+1;
-$cdid[]=$cmid;
-$clj[]=204;
-$npc[]=0;
-echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>显示设置</font></a>";
-echo "<font color=black>-</font>";
-
-//cmd及超链接值
-$cmid=$cmid+1;
-$cdid[]=$cmid;
-$clj[]=305;
-$npc[]=0;
-echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>还原快捷设置</font></a>";
-
-echo "<br>";
+    echo "<br>";
 
 
 
 
-////cmd及超链接值
-//$cmid=$cmid+1;
-//$cdid[]=$cmid;
-//$clj[]=299;
-//$npc[]=1;
-//echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>攻略大全</font></a>";
-//echo "<font color=black>-</font>";
+    ////cmd及超链接值
+    //$cmid=$cmid+1;
+    //$cdid[]=$cmid;
+    //$clj[]=299;
+    //$npc[]=1;
+    //echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>攻略大全</font></a>";
+    //echo "<font color=black>-</font>";
 //
-////cmd及超链接值
-//$cmid=$cmid+1;
-//$cdid[]=$cmid;
-//$clj[]=299;
-//$npc[]=2;
-//echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>在线客服</font></a>";
+    ////cmd及超链接值
+    //$cmid=$cmid+1;
+    //$cdid[]=$cmid;
+    //$clj[]=299;
+    //$npc[]=2;
+    //echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>在线客服</font></a>";
 //
-//echo "<br>";
+    //echo "<br>";
 
 
 
@@ -59,58 +56,47 @@ echo "<br>";
 
 
 
-echo "<br>";
+    echo "<br>";
 
 
-echo "<font color=black>【游戏选项】</font><br>";
+    echo "<font color=black>【游戏选项】</font><br>";
 
 
-//cmd及超链接值
-$cmid=$cmid+1;
-$cdid[]=$cmid;
-$clj[]=1;
-$npc[]=0;
-echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>游戏首页</font></a>";
-echo "<font color=black>-</font>";
+    //cmd及超链接值
+    $cmid=$cmid+1;
+    $cdid[]=$cmid;
+    $clj[]=675;
+    $npc[]=0;
+    echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=red>【SDK码兑换】</font></a>";
+    echo "<font color=black>-</font>";
 
-//cmd及超链接值
-$cmid=$cmid+1;
-$cdid[]=$cmid;
-$clj[]=687;
-$npc[]=0;
-echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>退出登录</font></a>";
+    //cmd及超链接值
+    $cmid=$cmid+1;
+    $cdid[]=$cmid;
+    $clj[]=687;
+    $npc[]=0;
+    echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=red>【退出登录】</font></a>";
 
-echo "<br>";
-
-
-
-//cmd及超链接值
-$cmid=$cmid+1;
-$cdid[]=$cmid;
-$clj[]=2;
-$npc[]=0;
-echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>返回游戏</font></a>"."<br>";
+    echo "<br>";
 
 
 
-
-
-
-echo "<font color=black>----------------------</font>"."<br>";
-//cmd及超链接值
-include("fhgame.php");
+    //cmd及超链接值
+    $cmid=$cmid+1;
+    $cdid[]=$cmid;
+    $clj[]=2;
+    $npc[]=0;
+    echo "<a href='xy.php?uid=$wjid&&cmd=$cmid&&sid=$a1'><font color=blue>返回游戏</font></a>"."<br>";
 
 
 
 
 
 
-
-
-
-
-
-} else{	
+    echo "<font color=black>----------------------</font>"."<br>";
+    //cmd及超链接值
+    include("fhgame.php");
+} else {
 }
 //解锁当前使用的ini
 include("./ini/jsini.php");
