2f31ee413e38fc78fe778f6e2bce16e7000dfbf1
kkajy0621 <811051558@qq.com>
2022-10-01 19:20:49 +0800

    补给背包添加返回首页

--- fqxy/admin/gm38.php
+++ fqxy/admin/gm38.php
@@ -33,15 +33,12 @@ for ($d=0;$d<300;$d++) {
         echo "<font color=red>恭喜所有玩家背包补给完毕</font>"."<br>";
         break;
     }
+    echo "<font color=black>---------------------</font>"."<br>";
+    echo "<a href='gm.php?gid=1'><font color=blue>【返回GM管理首页】</font></a>"."<br>";
+    echo "<br>";
+    echo "<font color=black>---------------------</font>"."<br>";
+    echo "<a href=http://".$xxjyurl."/admin/index.php><font color=blue>返回GM管理平台</font></a>"."<br>";
 }
-
-
-
-
-
-
-
-
 
 
 
