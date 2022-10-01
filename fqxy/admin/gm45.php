a21d4cd5687f452701cef465180336bd8f5c2d51
kkajy0621 <811051558@qq.com>
2022-10-01 18:30:15 +0800

    修复后台红包管理

--- fqxy/admin/gm45.php
+++ fqxy/admin/gm45.php
@@ -2,7 +2,7 @@
 
 
 
-$wjid=10000002;
+$wjid=10000001;
 //阻塞代码防止出现脏数据//自己的id锁
 $ininalock=$wjid."_lock".".txt";
 include("./ini/zsini.php");
@@ -16,62 +16,56 @@ include("./ini/all_lock.php");
 /////////公共锁
 
 
-if($zsspd==1&&$all_lock2==1){
+if ($zsspd==1&&$all_lock2==1) {
+    echo "<font color=red>【幻想西游红包管理】</font>"."<br>";
+    include("./sql/mysql.php");//调用数据库连接
+    $q2="all_xjhb";
+    mysql_query("set names utf8");
+    $str="select id from $q2";
+    $result=mysql_query($str) or die('SQL语句有误');
+
+    while (!!$row=mysql_fetch_array($result)) {
+        $id[]=$row['id'];
+        $m=$m+1;
+    }
+    if ($m >=1) {
+        $i=-1;
+        $ii=0;
+        for ($d=0;$d<$m;$d++) {
+            $ii=$ii+1;
+            $i=$i+1;
+            $sql1=mysql_query("select * from $q2 where id=$id[$i]", $conn);
+            $info1=@mysql_fetch_array($sql1);
+            $cz01=$info1['wjid'];
+            $cz02=$info1['wjmz'];
+            $cz03=$info1['wjje'];
+            $cz04=$info1['zh1'];
+            $cz05=$info1['zh2'];
+            echo $ii;
+            echo ".";
+            echo "（".$cz02."--".$cz01."）";
+            echo "（".$cz03."元）";
+            if ($cz04 !=111) {
+                echo "（支付宝:".$cz04."）";
+            } else {
+                echo "（无效）";
+            }
+            if ($cz05 !=111) {
+                echo "（微信:".$cz05."）";
+            } else {
+                echo "（无效）";
+            }
+            echo "<a href='gm.php?wjid=$wjiddd&&pass=$password&&gid=46&&hbid=$id[$i]'><font color=blue>【清理】</font></a>"."<br>";
 
 
+            echo "</br>";
+        }
 
-
-echo "<font color=red>【幻想西游红包管理】</font>"."<br>";
-include("./sql/mysql.php");//调用数据库连接 
-$q2="all_xjhb";
-mysql_query("set names utf8");
-$str="select id from $q2";
-$result=mysql_query($str) or die('SQL语句有误');
-
- while(!!$row=mysql_fetch_array($result)){
-$id[]=$row['id'];
-$m=$m+1;
-}
-if($m >=1){
-$i=-1;
-$ii=0;
-for($d=0;$d<$m;$d++){
-$ii=$ii+1;
-$i=$i+1;
-$sql1=mysql_query("select * from $q2 where id=$id[$i]",$conn);
-$info1=@mysql_fetch_array($sql1);
-$cz01=$info1['wjid'];
-$cz02=$info1['wjmz'];
-$cz03=$info1['wjje'];
-$cz04=$info1['zh1'];
-$cz05=$info1['zh2'];
-echo $ii;
-echo ".";
-echo "（".$cz02."--".$cz01."）";
-echo "（".$cz03."元）";
-if($cz04 !=111){
-echo "（支付宝:".$cz04."）";
-} else{
-echo "（无效）";	
-} 
-if($cz05 !=111){
-echo "（微信:".$cz05."）";
-} else{
-echo "（无效）";	
-} 
-echo "<a href='gm.php?wjid=$wjiddd&&pass=$password&&gid=46&&hbid=$id[$i]'><font color=blue>【清理】</font></a>"."<br>";
-
-
-echo "</br>";
-
-}
-
-echo "</br>";
-
-} else{
-echo "</br>";	
-echo "<font color=black>目前没有订单</font>"."<br>";
-} 
+        echo "</br>";
+    } else {
+        echo "</br>";
+        echo "<font color=black>目前没有订单</font>"."<br>";
+    }
 
 
 
@@ -84,16 +78,16 @@ echo "<font color=black>目前没有订单</font>"."<br>";
 
 
 
-echo "<br>";
-echo "<font color=black>---------------------</font>"."<br>";
-echo "<a href='gm.php?wjid=$wjiddd&&pass=$password&&gid=1'><font color=blue>【返回GM管理首页】</font></a>"."<br>";
+    echo "<br>";
+    echo "<font color=black>---------------------</font>"."<br>";
+    echo "<a href='gm.php?wjid=$wjiddd&&pass=$password&&gid=1'><font color=blue>【返回GM管理首页】</font></a>"."<br>";
 
-echo "<br>";
-echo "<font color=black>---------------------</font>"."<br>";
+    echo "<br>";
+    echo "<font color=black>---------------------</font>"."<br>";
 
 
-echo "<a href=".$xxjyurl."/admin/index.php?wjid=$wjiddd&pass=$password><font color=blue>返回GM管理平台</font></a>"."<br>";
-} else{	
+    echo "<a href=".$xxjyurl."/admin/index.php?wjid=$wjiddd&pass=$password><font color=blue>返回GM管理平台</font></a>"."<br>";
+} else {
 }
 
 
@@ -106,8 +100,3 @@ include("./ini/jszsini.php");
 //解锁当前使用的ini
 include("./ini/all_lock1.php");
 //解锁当前使用的ini
-
-
-
-
-?>
