40ab5b9e4057a26ae3805c234fb42aa8287b842d
kkajy0621 <811051558@qq.com>
2023-06-29 23:22:23 +0800

    修改分区服务器

--- fqxy/config/config.php
+++ fqxy/config/config.php
@@ -1,10 +1,10 @@
 <?php
 
 //分区所属家园地址
-$config['jy_url'] = 'http://127.0.0.1';
+$config['jy_url'] = 'http://47.95.37.39';
 
 //分区地址
-$config['xy_url'] = 'http://127.0.0.1';
+$config['xy_url'] = 'http://47.95.37.39';
 
 //分区数据库配置
 $config['mysql'] = [
